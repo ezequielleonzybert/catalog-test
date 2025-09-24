960d332cf1fb7e59401c09a686ea73868953e18f
Ezequiel <ezequielleonzybert@gmail.com>
2025-09-24 15:40:42 -0300

    pricemulti

--- docs/app.js
+++ docs/app.js
@@ -3,6 +3,9 @@ const categorias = document.getElementsByClassName('categorias')[0];
 const arrow = document.getElementById('arrow');
 let productDataArray = [];
 
+// Ajuste general de precios
+const PRICE_ADJUST = 1.15; // 15% de aumento global
+
 fetch('./data/products.csv')
     .then(response => response.text())
     .then(data => {
@@ -17,13 +20,9 @@ function parseCSV(data) {
     return rows
         .filter(row => row.trim() !== "") // Ignorar líneas vacías
         .map(row => {
-            // Expresión regular para dividir en comas, ignorando las dentro de comillas
             const regex = /(".*?"|[^,]+)(?=\s*,|\s*$)/g;
             const matches = [...row.matchAll(regex)].map(m => m[0]);
-
-            // Eliminar comillas externas si hay
             const cleanValues = matches.map(value => value.replace(/^"|"$/g, ''));
-
             const [name, price, description, category, tags] = cleanValues;
 
             return {
@@ -36,7 +35,6 @@ function parseCSV(data) {
         });
 }
 
-
 async function renderProducts(parsedProducts) {
     productCatalog.innerHTML = '';
     for (const product of parsedProducts) {
@@ -48,12 +46,12 @@ async function renderProducts(parsedProducts) {
         const formattedName = cleanText(product.name);
 
         const img = document.createElement('img');
-        img.src = `./images/${formattedName}/0.webp`
+        img.src = `./images/${formattedName}/0.webp`;
         img.alt = product.name;
 
         productElement.appendChild(img);
 
-        const formattedPrice = Number(product.price).toLocaleString('es-AR');
+        const formattedPrice = (Number(product.price) * PRICE_ADJUST).toLocaleString('es-AR');
         productElement.innerHTML += `
             <h2>${product.name}</h2>
             <p>$${formattedPrice}</p>
@@ -62,7 +60,6 @@ async function renderProducts(parsedProducts) {
         productCatalog.appendChild(productElement);
     }
 
-    // Una vez que los productos están en el DOM, verificamos la URL
     const params = new URLSearchParams(window.location.search);
     const productName = params.get('producto');
     if (productName) {
@@ -70,10 +67,9 @@ async function renderProducts(parsedProducts) {
     }
 }
 
-
 function filterAndSearchProducts() {
     const query = searchbox.value.toLowerCase().trim();
-    const keywords = query.split(/\s+/); // Divide en palabras separadas
+    const keywords = query.split(/\s+/);
 
     const selectedCategories = Array.from(document.querySelectorAll('.categoria input[type="checkbox"]:checked'))
         .map(checkbox => checkbox.getAttribute('data-category'));
@@ -81,26 +77,19 @@ function filterAndSearchProducts() {
     document.querySelectorAll('.product').forEach(product => {
         const tags = product.getAttribute('data-tags') || '';
         const productCategory = product.getAttribute('data-category');
-
-        // Verifica si todas las palabras están en los tags
         const matchesSearch = keywords.every(word => tags.includes(word));
-
-        // Verifica si el producto pertenece a una de las categorías seleccionadas
         const matchesCategory = selectedCategories.length === 0 || selectedCategories.includes(productCategory);
-
-        // Mostrar solo si cumple ambas condiciones
         product.style.display = matchesSearch && matchesCategory ? 'flex' : 'none';
     });
 }
 
-// Conectar la función a los eventos de búsqueda y selección de categorías
 const searchbox = document.getElementById('searchbox');
 searchbox.addEventListener('input', filterAndSearchProducts);
 document.querySelectorAll('.categoria input[type="checkbox"]').forEach(checkbox => {
     checkbox.addEventListener('change', filterAndSearchProducts);
 });
 
-//menu categorias
+// menu categorias
 let isHidden = true;
 
 function toggleCategorias() {
@@ -111,12 +100,10 @@ function toggleCategorias() {
     if (isHidden) {
         categorias.style.transform = "translateY(0)";
         productCatalog.style.transform = "translateY(0)";
-    }
-    else {
+    } else {
         categorias.style.transform = `translateY(20px)`;
         productCatalog.style.transform = `translateY(${categorias.clientHeight + 20}px)`;
     }
-
 }
 
 let prev_width = window.innerWidth;
@@ -125,16 +112,14 @@ let prev_categories_height = categorias.clientHeight;
 window.addEventListener('resize', () => {
     if (window.innerWidth > 800 && prev_width <= 800 && !isHidden) {
         toggleCategorias();
-    }
-    else if (categorias.clientHeight !== prev_categories_height && !isHidden) {
+    } else if (categorias.clientHeight !== prev_categories_height && !isHidden) {
         productCatalog.style.transform = `translateY(${categorias.clientHeight + 20}px)`;
         prev_categories_height = categorias.clientHeight;
     }
     prev_width = window.innerWidth;
 });
 
-//MODAL POPUP
-
+// MODAL POPUP
 const modal = document.querySelector('.modal');
 const modalContent = document.querySelector('.modal-content');
 const back = document.querySelector('#back');
@@ -164,7 +149,6 @@ async function loadImage(url) {
 
 async function loadGallery(formattedProductName) {
     const modalContent = document.querySelector('.modal-content');
-
     const loader = document.createElement('div');
     loader.classList.add('loader');
     modalContent.appendChild(loader);
@@ -178,8 +162,7 @@ async function loadGallery(formattedProductName) {
     while (true) {
         const imageUrl = `./images/${formattedProductName}/${index}.webp`;
         const loadedImage = await loadImage(imageUrl);
-
-        if (!loadedImage) break; // Si no se encuentra, termina el bucle
+        if (!loadedImage) break;
         const img = document.createElement("img");
         img.src = loadedImage;
         img.alt = `${formattedProductName} ${index}`;
@@ -190,65 +173,51 @@ async function loadGallery(formattedProductName) {
 
     loader.remove();
 
-    // Crear el contenedor de puntos
     const dotsContainer = document.createElement('div');
     dotsContainer.classList.add('dots-container');
 
-    // Crear los puntos según la cantidad de imágenes
     images.forEach((_, i) => {
         const dot = document.createElement('span');
         dot.classList.add('dot');
-        if (i === 0) dot.classList.add('active'); // Marcar el primer punto como activo
+        if (i === 0) dot.classList.add('active');
         dotsContainer.appendChild(dot);
     });
 
-    // Insertar la galería y los puntos en el modal
     modalContent.prepend(galleryContainer);
     modalContent.appendChild(dotsContainer);
 
-    // Actualizar los puntos al desplazarse
     galleryContainer.addEventListener('scroll', () => {
         const scrollPosition = galleryContainer.scrollLeft;
         const imageWidth = galleryContainer.clientWidth;
         const activeIndex = Math.round(scrollPosition / imageWidth);
-
-        // Actualizar los puntos
         const dots = dotsContainer.querySelectorAll('.dot');
         dots.forEach((dot, i) => {
-            if (i === activeIndex) {
-                dot.classList.add('active');
-            } else {
-                dot.classList.remove('active');
-            }
+            dot.classList.toggle('active', i === activeIndex);
         });
     });
 
-    // Permitir hacer clic en los puntos para desplazarse
     dotsContainer.querySelectorAll('.dot').forEach((dot, i) => {
         dot.addEventListener('click', () => {
             const imageWidth = galleryContainer.clientWidth;
             galleryContainer.scrollTo({
                 left: i * imageWidth,
-                behavior: 'smooth', // Desplazamiento suave
+                behavior: 'smooth',
             });
         });
     });
 
-    //ARROW-SIDE
     fetch("./images/icons/arrow-side.svg")
         .then(res => res.text())
         .then(data => {
             let arrowRight = new DOMParser().parseFromString(data, "image/svg+xml").querySelector("svg");
             let arrowLeft = new DOMParser().parseFromString(data, "image/svg+xml").querySelector("svg");
             arrowRight.classList.add("arrow-side");
-            arrowLeft.classList.add("arrow-side", "arrow-left")
-            document.querySelector(".modal-content").appendChild(arrowRight);
-            document.querySelector(".modal-content").appendChild(arrowLeft);
+            arrowLeft.classList.add("arrow-side", "arrow-left");
+            modalContent.appendChild(arrowRight);
+            modalContent.appendChild(arrowLeft);
 
-            // Agregar event listener después de insertar las flechas
             modalContent.addEventListener("click", (event) => {
-                const scrollAmount = galleryContainer.clientWidth; // Se mueve por el ancho de la galería
-
+                const scrollAmount = galleryContainer.clientWidth;
                 if (event.target.closest(".arrow-side")) {
                     if (event.target.closest(".arrow-left")) {
                         galleryContainer.scrollBy({ left: -scrollAmount });
@@ -259,7 +228,6 @@ async function loadGallery(formattedProductName) {
             });
         })
         .catch(err => console.error("Error:", err));
-
 }
 
 function openModal(productData) {
@@ -267,18 +235,18 @@ function openModal(productData) {
 
     const modalContent = document.querySelector('.modal-content');
 
-    loadGallery(cleanText(productData.name));
-
-    const formattedPrice = Number(productData.price).toLocaleString('es-AR');
+    // primero info del producto
     modalContent.innerHTML = `
         <div class="product-info">
             <h2>${productData.name}</h2>
-            <p class="price">$${formattedPrice}</p>
+            <p class="price">$${(Number(productData.price) * PRICE_ADJUST).toLocaleString('es-AR')}</p>
             <p class="description">${productData.description}</p>
         </div>
     `;
 
-    // Mostrar el modal
+    // después la galería
+    loadGallery(cleanText(productData.name));
+
     scrollY = window.scrollY;
     document.body.style.top = `-${scrollY}px`;
     document.body.style.paddingRight = `${window.innerWidth - document.body.clientWidth}px`;
@@ -290,16 +258,12 @@ function openModal(productData) {
         modal.classList.add('appear');
     });
 
-    // Guardar en la URL
     history.pushState({ modalOpen: true }, '', `?producto=${cleanText(productData.name)}`);
 }
 
 function openModalFromURL(productName) {
     const productData = productDataArray.find(p => cleanText(p.name) === cleanText(productName));
-
-    if (productData) {
-        openModal(productData);
-    }
+    if (productData) openModal(productData);
 }
 
 function closeModal() {
@@ -307,16 +271,14 @@ function closeModal() {
     document.body.style.paddingRight = `0`;
     document.body.classList.remove('no-scroll');
 
-
     modal.addEventListener('transitionend', () => {
         modal.classList.add('hidden');
-
     }, { once: true });
 
     window.scrollTo(0, scrollY);
 
     if (history.state?.modalOpen) {
-        history.replaceState(null, '', window.location.pathname); // Elimina el parámetro sin afectar el historial
+        history.replaceState(null, '', window.location.pathname);
     }
 }
 
@@ -326,12 +288,11 @@ modal.addEventListener('click', (event) => {
     }
 });
 
-window.addEventListener("popstate", (event) => {
+window.addEventListener("popstate", () => {
     const params = new URLSearchParams(window.location.search);
     const productName = params.get('producto');
-
     if (productName) {
-        openModalFromURL(productName); // Reabre el modal si hay un producto en la URL
+        openModalFromURL(productName);
     } else {
         closeModal();
     }
@@ -339,88 +300,8 @@ window.addEventListener("popstate", (event) => {
 
 function cleanText(texto) {
     return texto
-        .normalize("NFD") // Descompone caracteres con tildes
-        .replace(/[\u0300-\u036f]/g, "") // Elimina las tildes
-        .replace(/\s+/g, "_") // Elimina los espacios
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+        .replace(/\s+/g, "_")
         .toLowerCase();
 }
-
-// function enableDragScroll(galleryContainer) {
-//     let isDragging = false;
-//     let startX, scrollLeft;
-
-//     galleryContainer.addEventListener('mousedown', (e) => {
-//         isDragging = true;
-//         startX = e.pageX;
-//         scrollLeft = galleryContainer.scrollLeft;
-//         galleryContainer.style.cursor = 'grabbing';
-//         e.preventDefault();
-//     });
-
-//     galleryContainer.addEventListener('mouseleave', () => {
-//         if (isDragging) {
-//             isDragging = false;
-//             galleryContainer.style.cursor = 'grab';
-//             snapToNearestImage(galleryContainer)
-//         }
-//     });
-
-//     galleryContainer.addEventListener('mouseup', () => {
-//         if (isDragging) {
-//             isDragging = false;
-//             galleryContainer.style.cursor = 'grab';
-//             snapToNearestImage(galleryContainer)
-//         }
-//     });
-
-//     galleryContainer.addEventListener('mousemove', (e) => {
-//         if (!isDragging) return;
-//         e.preventDefault();
-//         const x = e.pageX;
-//         const walk = (x - startX) * 1.5;
-//         galleryContainer.scrollLeft = scrollLeft - walk;
-//     });
-// }
-
-// function snapToNearestImage(galleryContainer) {
-//     const images = galleryContainer.querySelectorAll('img');
-//     const galleryScrollLeft = galleryContainer.scrollLeft;
-//     const galleryWidth = galleryContainer.clientWidth;
-
-//     let closestImage = null;
-//     let closestDistance = Infinity;
-
-//     // Encuentra la imagen más cercana al scroll actual
-//     images.forEach((img, index) => {
-//         const imgLeft = index * galleryWidth; // Posición de la imagen
-//         const distance = Math.abs(galleryScrollLeft - imgLeft);
-
-//         if (distance < closestDistance) {
-//             closestDistance = distance;
-//             closestImage = img;
-//         }
-//     });
-
-//     // Ajusta el scroll a la imagen más cercana
-//     if (closestImage) {
-//         const targetScrollLeft = Array.from(images).indexOf(closestImage) * galleryWidth;
-//         galleryContainer.scrollTo({
-//             left: targetScrollLeft,
-//             behavior: 'smooth', // Desplazamiento suave
-//         });
-//     }
-// }
-
-//LOADER ANIMATION
-
-// window.addEventListener('load', () => {
-//     setTimeout(() => {
-//         const pageLoader = document.getElementById('page-loader');
-
-//         // Oculta el loader
-//         pageLoader.style.display = 'none';
-
-//         // Muestra el contenido
-//         document.body.classList.remove('hidden');
-//     }, 1500); // 2000 milisegundos = 2 segundos
-// });
